a56141c0fc3e5ad046ca0dced43525c1b85c4953
Ruikang Tao <rico00121@outlook.com>
2025-06-03 22:00:05 +0300

    refactor: add server-only

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,7 @@
 import { Database } from '@/types/supabase'
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-
+import'server-only';
 export async function createClient() {
   const cookieStore = await cookies()
 
