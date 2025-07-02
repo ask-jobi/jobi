9fa08132397a842ed5142267aac9b4eb94950b87
return764 <543851436@qq.com>
2025-07-02 21:00:48 +0800

    feat: refactor node diff

--- components/blocks/editor-00/nodes.ts
+++ components/blocks/editor-00/nodes.ts
@@ -12,8 +12,15 @@ import {
 } from "lexical"
 import {HorizontalRuleNode} from "@lexical/react/LexicalHorizontalRuleNode";
 import {AutoLinkNode, LinkNode} from "@lexical/link";
-import {$isListItemNode, $isListNode, ListItemNode, ListNode} from "@lexical/list";
-import {$convertFromMarkdownString} from "@lexical/markdown";
+import {
+  $createListItemNode,
+  $createListNode,
+  $isListItemNode,
+  $isListNode,
+  ListItemNode,
+  ListNode
+} from "@lexical/list";
+import {$convertFromMarkdownString, $convertToMarkdownString} from "@lexical/markdown";
 import {SUPPORT_TRANSFORMER} from "@/components/blocks/editor-00/plugins";
 import {diffWords} from "diff";
 
@@ -111,6 +118,109 @@ const replaceDiffNode = (node: LexicalNode, diffState: DiffStates) => {
   }
 }
 
+function processElementNode(
+  oldNode: ElementNode,
+  newNode: ElementNode
+): ElementNode[] {
+  const resultNodes: ElementNode[] = []
+  const oldChildNodes = oldNode.getChildren()
+  const newChildNodes = newNode.getChildren()
+
+  let oldIdx = 0;
+  let newIdx = 0;
+  while (oldIdx < oldChildNodes.length || newIdx < newChildNodes.length) {
+    const oldChildNode = oldChildNodes[oldIdx];
+    const newChildNode = newChildNodes[newIdx];
+
+    if (oldChildNode && newChildNode && oldChildNode.getType() === newChildNode.getType()) {
+      // 代表类型一致的节点
+      if ($isListNode(oldChildNode) && $isListNode(newChildNode)) {
+        // list节点的子节点只可能时listItem，因此这里直接递归交给下一个分支处理
+        const recursionResult = processElementNode(oldChildNode, newChildNode)
+        const listNode = $createListNode(oldChildNode.getListType())
+        listNode.append(...recursionResult)
+        resultNodes.push(listNode)
+      } else if ($isListItemNode(oldChildNode) && $isListItemNode(newChildNode)) {
+        // listItem节点的子节点只能是叶子节点
+        const diffTextNodes = processTextNodes(oldChildNode.getAllTextNodes(), newChildNode.getAllTextNodes())
+        const listItemNode = $createListItemNode()
+        listItemNode.append(...diffTextNodes)
+        resultNodes.push(listItemNode)
+      } else if ($isParagraphNode(oldChildNode) && $isParagraphNode(newChildNode)) {
+        // paragraph的子节点也只能是叶子节点
+        const diffTextNodes = processTextNodes(oldChildNode.getAllTextNodes(), newChildNode.getAllTextNodes())
+        const paragraphNode = $createParagraphNode()
+        paragraphNode.append(...diffTextNodes)
+        resultNodes.push(paragraphNode)
+      } else {
+        //不满足以上条件的是暂不支持的节点，那么diff时简单判断其content是否相同即可
+        const oldContent = oldChildNode.getTextContent()
+        const newContent = newChildNode.getTextContent()
+        if (oldContent !== newContent) {
+          const removedPara = $createParagraphNode();
+          removedPara.append($createDiffTextNode(oldContent, 'removed'));
+          resultNodes.push(removedPara);
+
+          const addedPara = $createParagraphNode();
+          addedPara.append($createDiffTextNode(newContent, 'added'));
+          resultNodes.push(addedPara);
+        } else {
+          resultNodes.push(newChildNode as ElementNode);
+        }
+      }
+
+      oldIdx ++
+      newIdx ++
+    } else if (oldChildNode && (!newChildNode || oldChildNode.getType() !== newChildNode.getType())) {
+      // 2. 旧节点存在，但新节点不存在 或 类型不同 -> 标记为“removed”
+      replaceDiffNode(oldChildNode, "removed")
+      resultNodes.push(oldChildNode as ElementNode);
+      oldIdx++;
+    } else if (newChildNode && (!oldChildNode || oldChildNode.getType() !== newChildNode.getType())) {
+      // 3. 新节点存在，但旧节点不存在 或 类型不同 -> 标记为“added”
+      replaceDiffNode(newChildNode, "added")
+      resultNodes.push(newChildNode as ElementNode);
+      newIdx++;
+    }
+  }
+
+  return resultNodes
+}
+
+function processTextNodes(
+  oldTextNodes: TextNode[],
+  newTextNodes: TextNode[]
+): TextNode[] {
+  // 这个方法的目的是逐个对比所有的TextNode?
+  // 考虑合并对比。
+  const result: TextNode[] = []
+  const oldParagraph = $createParagraphNode()
+  const newParagraph = $createParagraphNode()
+  oldParagraph.append(...oldTextNodes)
+  newParagraph.append(...newTextNodes)
+
+  const tempParagraph1 = $createParagraphNode()
+  const tempParagraph2 = $createParagraphNode()
+  tempParagraph1.append(oldParagraph)
+  tempParagraph2.append(newParagraph)
+
+  const oldContent = $convertToMarkdownString(SUPPORT_TRANSFORMER, tempParagraph1)
+  const newContent = $convertToMarkdownString(SUPPORT_TRANSFORMER, tempParagraph2)
+
+  const diffContents = diffWords(oldContent, newContent)
+  diffContents.forEach(item => {
+    const tempNode = $createParagraphNode()
+    $convertFromMarkdownString(item.value, SUPPORT_TRANSFORMER, tempNode)
+    replaceDiffNode(tempNode, item.added ? 'added' : item.removed ? 'removed' : 'unchanged')
+    const innerParagraphNode = tempNode.getFirstChild()
+    if (innerParagraphNode && $isParagraphNode(innerParagraphNode)) {
+      result.push(...(innerParagraphNode.getChildren() as TextNode[]));
+    }
+  })
+
+  return result
+}
+
 export function $calculateDiffWords(newMarkdown: string, node: ElementNode = $getRoot()) {
   const editor = $getEditor()
 
@@ -118,119 +228,10 @@ export function $calculateDiffWords(newMarkdown: string, node: ElementNode = $ge
     const tempRoot = $createParagraphNode()
     $convertFromMarkdownString(newMarkdown, SUPPORT_TRANSFORMER, tempRoot)
 
-    const oldNodes = node.getChildren()
-    const newNodes = tempRoot.getChildren()
+    // 从根节点开始处理差异
+    const finalDiffedNodes = processElementNode(node, tempRoot);
 
     node.clear();
-
-    function processElementNodes(
-      oldChildNodes: Array<LexicalNode>,
-      newChildNodes: Array<LexicalNode>
-    ): Array<LexicalNode> {
-      const resultNodes: Array<LexicalNode> = [];
-      let oldIdx = 0;
-      let newIdx = 0;
-
-      while (oldIdx < oldChildNodes.length || newIdx < newChildNodes.length) {
-        const oldChildNode = oldChildNodes[oldIdx];
-        const newChildNode = newChildNodes[newIdx];
-
-        if (oldChildNode && newChildNode && oldChildNode.getType() === newChildNode.getType()) {
-          // 1. 节点类型相同
-          if ($isListNode(oldChildNode) && $isListNode(newChildNode)) {
-            // 如果是 ListNode，递归处理其子 ListItemNode
-            const newListNode = new ListNode(newChildNode.getListType(), newChildNode.getStart());
-            const diffedListItems = processElementNodes(
-              oldChildNode.getChildren(),
-              newChildNode.getChildren()
-            );
-            diffedListItems.forEach(item => newListNode.append(item as ListItemNode));
-            resultNodes.push(newListNode);
-          } else if ($isListItemNode(oldChildNode) && $isListItemNode(newChildNode)) {
-            // 如果是 ListItemNode，比较其内部的文本内容
-            // ListItemNode 的子节点通常是 ParagraphNode 或其他 inline 节点
-            const oldItemContent = oldChildNode.getTextContent();
-            const newItemContent = newChildNode.getTextContent();
-
-            const newListItem = new ListItemNode(newChildNode.getValue()); // 保留原有的 value
-
-            if (oldItemContent === newItemContent) {
-              // 内容相同，直接使用新节点（保持其原有结构）
-              newListItem.append(...newChildNode.getChildren());
-              resultNodes.push(newListItem);
-            } else {
-              // 内容不同，进行词级别 diff TODO 这里存在一个问题，因为直接对比的listitem的content，所以创建出来的节点会有格式丢失的问题
-              const diffResult = diffWords(oldItemContent, newItemContent);
-              diffResult.forEach(item => {
-                const diffText = $createDiffTextNode(item.value,
-                  item.added ? 'added' : item.removed ? 'removed' : 'unchanged'
-                );
-                // 这里需要将 DiffTextNode 包裹在 ParagraphNode 中，因为 ListItemNode 通常包含 ParagraphNode
-                // 如果 ListItemNode 的子节点不是 ParagraphNode，则需要更复杂的逻辑来重建结构
-                const lastNode = newListItem.getLastChild()
-                if ($isParagraphNode(lastNode)) {
-                  lastNode?.append(diffText);
-                } else {
-                  const newPara = $createParagraphNode();
-                  newPara.append(diffText);
-                  newListItem.append(newPara);
-                }
-              });
-              resultNodes.push(newListItem);
-            }
-          } else if ($isParagraphNode(oldChildNode) && $isParagraphNode(newChildNode)) {
-            // 已经是段落节点，进行词级别 diff
-            const oldText = oldChildNode.getTextContent();
-            const newText = newChildNode.getTextContent();
-
-            if (oldText === newText) {
-              // 内容相同，直接使用新节点
-              resultNodes.push(newChildNode);
-            } else {
-              const newParagraph = $createParagraphNode();
-              const diffResult = diffWords(oldText, newText);
-              diffResult.forEach(item => {
-                // TODO 如上TODO
-                newParagraph.append($createDiffTextNode(item.value,
-                  item.added ? 'added' : item.removed ? 'removed' : 'unchanged'
-                ));
-              });
-              resultNodes.push(newParagraph);
-            }
-          } else {
-            // 其他未知或不支持详细diff的相同类型 ElementNode
-            // 简单处理：如果文本内容不同，则视为整个节点被替换
-            if (oldChildNode.getTextContent() !== newChildNode.getTextContent()) {
-              const removedPara = $createParagraphNode();
-              removedPara.append($createDiffTextNode(oldChildNode.getTextContent(), 'removed'));
-              resultNodes.push(removedPara);
-
-              const addedPara = $createParagraphNode();
-              addedPara.append($createDiffTextNode(newChildNode.getTextContent(), 'added'));
-              resultNodes.push(addedPara);
-            } else {
-              resultNodes.push(newChildNode); // 内容相同，直接保留
-            }
-          }
-          oldIdx++;
-          newIdx++;
-        } else if (oldChildNode && (!newChildNode || oldChildNode.getType() !== newChildNode.getType())) {
-          // 2. 旧节点存在，但新节点不存在 或 类型不同 -> 标记为“removed”
-          replaceDiffNode(oldChildNode, "removed")
-          resultNodes.push(oldChildNode);
-          oldIdx++;
-        } else if (newChildNode && (!oldChildNode || oldChildNode.getType() !== newChildNode.getType())) {
-          // 3. 新节点存在，但旧节点不存在 或 类型不同 -> 标记为“added”
-          replaceDiffNode(newChildNode, "added")
-          resultNodes.push(newChildNode);
-          newIdx++;
-        }
-      }
-      return resultNodes;
-    }
-
-    // 从根节点开始处理差异
-    const finalDiffedNodes = processElementNodes(oldNodes, newNodes);
 
     // 将所有差异化处理后的节点添加到编辑器中
     finalDiffedNodes.forEach(p => {
