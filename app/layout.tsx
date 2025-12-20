7fbe639b0e7e23059cde193a9655d70cbeefb481
Ruikang Tao <rico00121@outlook.com>
2025-12-20 14:21:26 +0800

    [AIRS-89] feat: add tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import {Toaster} from "@/components/ui/sonner";
 import {NextIntlClientProvider} from "next-intl";
 import {getUserLocale} from "@/lib/i18n/services";
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +44,11 @@ export default async function RootLayout({
           {children}
           <Toaster />
         </NextIntlClientProvider>
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="dd508046-2ce0-4c69-bcd8-f2ee1ee50eba"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
